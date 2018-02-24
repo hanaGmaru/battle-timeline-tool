253be39c0995b4d27c49451d542e6a8a881e5807
Annie Scarlet <take.gn+annie@gmail.com>
2018-02-25 01:20:04 +0900

    日本語テキスト表示

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <div v-for="skill in skill_data.common">
+  <div v-if="langText">
+    <div v-for="skill in skill_data.common" v-bind:key="'common-action:' + skill.id">
       <img v-bind:src="'/static/img/skill_icon/' + skill.icon" />
-      <div v-for="action in skill.actions">
+      <div v-for="action in skill.actions" v-bind:key="'action:' + action.id">
         <img v-bind:src="'/static/img/skill_icon/' + action.icon" />
+        <div>
+          <span> {{ langText.action[action.id] }} </span>
+          <span> {{ langText.type[action.type_id] }} </span>
+          <span> {{ langText.content[action.content_id] }} </span>
+        </div>
         <div> {{action}} </div>
       </div>
     </div>
 
-    <div v-for="skill in skill_data.only">
+    <div v-for="skill in skill_data.only" v-bind:key="'only-action:' + skill.id">
       <img v-bind:src="'/static/img/skill_icon/' + skill.icon" />
-      <div v-for="action in skill.actions">
+      <div v-for="action in skill.actions" v-bind:key="'action:' + action.id">
         <img v-bind:src="'/static/img/skill_icon/' + action.icon" />
+        <div>
+          <span> {{ langText.action[action.id] }} </span>
+          <span> {{ langText.type[action.type_id] }} </span>
+          <span> {{ langText.content[action.content_id] }} </span>
+        </div>
         <div> {{action}} </div>
       </div>
     </div>
@@ -20,17 +30,27 @@
 
 <script>
 import skillData from '@/assets/skill_data'
+import api from '@/api'
 
 export default {
   // name: 'HelloWorld',
   data () {
     return {
-      // msg: 'Welcome to Your Vue.js App'
+      langText: null
     }
   },
   computed: {
     skill_data () {
       return skillData
+    }
+  },
+  async mounted () {
+    let v = await api.loadTextData()
+    this.langText = v.data
+  },
+  methods: {
+    getActionText (id) {
+      return this.langText['action'][id]
     }
   }
 }
